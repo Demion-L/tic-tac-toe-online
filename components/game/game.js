543e5507f8a898c7e17139f4cdb6060eb04dc842
Demion-L <padovds@ukr.net>
2024-06-07 09:20:18 +0300

    tailwind implemented

--- components/game/game.js
+++ components/game/game.js
@@ -1,8 +1,7 @@
-import { GameCell } from './game-cell'
-import { GameInfo } from './game-info'
-import styles from './game.module.css'
-import { useGameState } from './use-game-state';
-
+import { GameCell } from "./game-cell";
+import { GameInfo } from "./game-info";
+import styles from "./game.module.css";
+import { useGameState } from "./use-game-state";
 
 export function Game() {
   const {
@@ -12,17 +11,17 @@ export function Game() {
     isDraw,
     resetGame,
     toggleCell,
-    getWinnerCell
+    getWinnerCell,
   } = useGameState();
 
   return (
-    <div className={styles['game']}>
+    <div className='flex flex-column items-center w-40 mx-auto my-24 p-5 border border-black'>
       <GameInfo
         isDraw={isDraw}
         winnerSymbol={winnerSymbol}
         currentStep={currentStep}
       />
-      <div className={styles["game-field"]}>
+      <div className='grid pt-px pl-px'>
         {cells.map((symbol, index) => (
           <GameCell
             key={index}
@@ -32,7 +31,11 @@ export function Game() {
           />
         ))}
       </div>
-      <button className={styles["reset"]} onClick={resetGame}>Сбросить</button>
+      <button
+        className='cursor-pointer mt-2.5 bg-transparent border border-gray-400 py-1 px-3 rounded'
+        onClick={resetGame}>
+        Сбросить
+      </button>
     </div>
-  )
+  );
 }
